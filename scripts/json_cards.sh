ca55cba9eeceb6716c34c4bb327762006bfd86d0
ludeeus <joasoe@gmail.com>
2018-08-12 02:22:44 +0200

    initital

---
+++ scripts/json_cards.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+#Author: Joakim Sørensen @ludeeus
+
+
+git config --global user.name "$GH_USER" || exit 1
+git config --global user.email "$GH_MAIL" || exit 1
+
+mkdir /publish
+cd /publish || exit 1
+git init
+git remote add origin https://$GH_USER:$GH_API@github.com/custom-cards/information.git || exit 1
+git fetch --all && git reset --hard origin/master
+
+jsondata=$(curl -u "$GH_USER:$GH_API" -sSL https://api.github.com/orgs/custom-cards/repos | jq -r .)
+jsonfile='/publish/repos.json'
+
+if [ -f $jsonfile ];then
+  rm $jsonfile
+fi
+
+echo "{" >> $jsonfile
+echo $jsondata
+for row in $(echo "${jsondata}" | jq -r 'sort_by(.name)[] | @base64'); do
+  _jq() {
+  echo ${row} | base64 --decode | jq -r ${1}
+  }
+  name=$(_jq '.name')
+  archived=$(_jq '.archived')
+  echo "Generating json for $name"
+  updated_at=$(_jq '.updated_at')
+  base_url='https://raw.githubusercontent.com/custom-cards/'
+  url=$base_url$name'/master/'
+    live=$(curl -sSL $url'VERSION')
+    test=$(echo $live | grep "404: Not Found")
+    if [[ -z "$test" ]];then
+      version=$(curl -sSL $url'VERSION')
+      remote_location=$base_url$name'/master/'$name'.js'
+      changelog=$(curl -sSL $url'changelog.md')
+      test=$(echo $changelog | grep "404: Not Found")
+      if [[ ! -z "$test" ]];then
+        changelog='https://github.com/custom-cards/'$name'/releases/latest'
+      else
+        changelog='https://github.com/custom-cards/'$name'/blob/master/changelog.md'
+      fi
+      visitrepo='https://github.com/custom-cards/'$name
+    fi
+  if [[ "$name" == "information" ]]; then
+    echo Nothing to see here
+  elif [[ "$name" == "" ]]; then
+    echo Nothing to see here
+  else
+    cat >> $jsonfile <<EOF
+    "$name": {
+      "updated_at": "${updated_at::10}",
+      "version": "$version",
+      "remote_location": "$remote_location",
+      "visit_repo": "$visitrepo",
+      "changelog": "$changelog"
+    },
+EOF
+  fi
+done
+
+mkdir /ciotlosm
+cd /ciotlosm || exit 1
+git init
+git remote add origin https://github.com/ciotlosm/custom-lovelace.git
+git fetch --all && git reset --hard origin/master
+baseurl='https://raw.githubusercontent.com/ciotlosm/custom-lovelace/master/'
+for D in `find . -maxdepth 1 -type d`;do
+    DIR=$(echo $D | awk -F'/' '{print $2}')
+    if [[ "$DIR" == ".git" ]]; then
+      echo "nothing to see here"
+    elif [[ "$DIR" == "." ]]; then
+      echo "nothing to see here"
+    elif [[ "$DIR" == "" ]]; then
+      echo "nothing to see here"
+    else
+      version=$(cat './'${D}'/VERSION')
+      download=$baseurl$DIR'/'$DIR'.js'
+      changelog='https://github.com/ciotlosm/custom-lovelace/tree/master/'$DIR'/changelog.md'
+      visitrepo='https://github.com/ciotlosm/custom-lovelace/tree/master/'$DIR
+      test=$(echo '"'$DIR'"')
+      if ! grep -q $test $jsonfile; then
+        echo adding $DIR
+        cat >> $jsonfile <<EOF
+        "$DIR": {
+          "updated_at": "Unknown",
+          "version": "$version",
+          "remote_location": "$download",
+          "visit_repo": "$visitrepo",
+          "changelog": "$changelog"
+        },
+EOF
+      else
+        echo skipping $DIR
+      fi
+    fi
+done
+
+
+cd /publish || exit 1
+sed -i '$ s/.$//' $jsonfile
+echo "}" >> $jsonfile
+cat $jsonfile
+count=$(wc -l $jsonfile | awk -F' ' '{print $1}')
+if [[ $count -gt 10 ]]; then
+  git add .
+  git commit -m "Update repos.json"
+  git push -u origin master
+else
+  exit 0
+fi
---
+++ scripts/json_components.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#Author: Joakim Sørensen @ludeeus
+
+
+git config --global user.name "$GH_USER" || exit 1
+git config --global user.email "$GH_MAIL" || exit 1
+
+mkdir /publish
+cd /publish || exit 1
+git init
+git remote add origin https://$GH_USER:$GH_API@github.com/custom-components/information.git || exit 1
+git fetch --all && git reset --hard origin/master
+
+jsondata=$(curl -u "$GH_USER:$GH_API" -sSL https://api.github.com/orgs/custom-components/repos | jq -r .)
+jsonfile='./repos.json'
+
+if [ -f $jsonfile ];then
+  rm $jsonfile
+fi
+
+echo "{" >> $jsonfile
+echo $jsondata
+for row in $(echo "${jsondata}" | jq -r 'sort_by(.name)[] | @base64'); do
+  _jq() {
+  echo ${row} | base64 --decode | jq -r ${1}
+  }
+  name=$(_jq '.name')
+  archived=$(_jq '.archived')
+  if [[ "$name" == "information" ]]; then
+    echo "Nothing to see here..."
+  elif [[ "$archived" == "true" ]]; then
+    echo "Nothing to see here..."
+  else
+    echo "Generating json for $name"
+    updated_at=$(_jq '.updated_at')
+    #get version:
+    base_url='https://raw.githubusercontent.com/custom-components/'
+    if [[ "$name" == *"."*  ]]; then
+      sub_dir=$(echo $name | awk -F'.' '{print $1}')'/'
+      file=$(echo $name | awk -F'.' '{print $2}')
+    else
+      sub_dir=''
+      file=$name
+    fi
+      url=$base_url$name'/master/custom_components/'$sub_dir$file'.py'
+      live=$(curl -sSL $url)
+      test=$(echo $live | grep "404: Not Found")
+      if [[ -z "$test" ]];then
+        version=$(echo $live | grep "__version__ " | head -n 1 | awk -F"'" '{print $2}')
+        local_location='/custom_components/'$sub_dir$file'.py'
+        remote_location=$url
+        visitrepo='https://github.com/custom-components/'$name
+        changelog=$visitrepo'/releases/latest'
+      else
+        url=$base_url$name'/master/custom_components/'$sub_dir$file'/__init__.py'
+        live=$(curl -sSL $url)
+        version=$(echo $live | grep "__version__ " | head -n 1 | awk -F"'" '{print $2}')
+        local_location='/custom_components/'$sub_dir$file'/__init__.py'
+        remote_location=$url
+        visitrepo='https://github.com/custom-components/'$name
+        changelog=$visitrepo'/releases/latest'
+      fi
+    cat >> $jsonfile <<EOF
+    "$name": {
+      "updated_at": "${updated_at::10}",
+      "version": "$version",
+      "local_location": "$local_location",
+      "remote_location": "$remote_location",
+      "visit_repo": "$visitrepo",
+      "changelog": "$changelog"
+    },
+EOF
+  fi
+done
+sed -i '$ s/.$//' $jsonfile
+echo "}" >> $jsonfile
+cat $jsonfile
+count=$(wc -l $jsonfile | awk -F' ' '{print $1}')
+if [[ $count -gt 10 ]]; then
+  git add .
+  git commit -m "Update repos.json"
+  git push -u origin master
+else
+  exit 0
+fi
